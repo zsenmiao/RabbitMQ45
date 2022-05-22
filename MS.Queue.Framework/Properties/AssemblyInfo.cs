40aa2707b57187aa28a1e88c9b196b7d0b61d342
森淼 周 <1073376250@qq.com>
2022-05-22 11:22:40 +0800

    update

--- MS.Queue.Framework/Properties/AssemblyInfo.cs
+++ MS.Queue.Framework/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("MS.Queue.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("RabbitMQ帮助类")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mushui")]
 [assembly: AssemblyProduct("MS.Queue.Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
